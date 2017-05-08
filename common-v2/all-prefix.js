875ae0952e00145251ea88eb1886897836182b7c
Danh Luong <dluong@pingidentity.com>
2017-05-08 14:14:49 +0700

    BE-2268: prevent jQueryBE to be register in AMD

--- common-v2/all-prefix.js
+++ common-v2/all-prefix.js
@@ -1,3 +1,8 @@
+// BE-2268
+// Prevent jQueryBE to be register in AMD
+// which cause conflict when client site use RequireJS
+var tmpDefine = define; define = undefined;
+
 (function () {
     if (document.all && !document.addEventListener) {
         console.log('[PingOne] IE8 and below are not supported!');
--- common-v2/all-suffix.js
+++ common-v2/all-suffix.js
@@ -1,3 +1,7 @@
 
 // END concatenate_files
 })();
+
+// BE-2268
+// Restore [define] to it original
+define = tmpDefine;
