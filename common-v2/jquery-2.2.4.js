d3115e6dba2a04854bf335fbf650890b7a621c9e
NGOC HONG NGUYEN <nnguyen@pingidentity.com>
2017-09-26 11:44:01 +0700

    BE-395: [Risk|Firefox] Implement long-term solution to bypass FF validation (global_eval() function)

--- common-v2/jquery-2.2.4.js
+++ common-v2/jquery-2.2.4.js
@@ -319,6 +319,32 @@ jQuery.extend( {
 			typeof obj;
 	},
 
+	// Evaluates a script in a global context
+	globalEval: function( code ) {
+		var script,
+			indirect = eval;
+
+		code = jQuery.trim( code );
+
+		if ( code ) {
+
+			// If the code includes a valid, prologue position
+			// strict mode pragma, execute code by injecting a
+			// script tag into the document.
+			if ( code.indexOf( "use strict" ) === 1 ) {
+				script = document.createElement( "script" );
+				script.text = code;
+				document.head.appendChild( script ).parentNode.removeChild( script );
+			} else {
+
+				// Otherwise, avoid the DOM node creation, insertion
+				// and removal by using an indirect global eval
+
+				indirect( code );
+			}
+		}
+	},
+
 	// Convert dashed to camelCase; used by the css and data modules
 	// Support: IE9-11+
 	// Microsoft forgot to hump their vendor prefix (#9572)
