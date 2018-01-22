1bcd1a33e9c73d0f80e0502fc0a487e1aab1e3fe
NGOC HONG NGUYEN <nnguyen@pingidentity.com>
2018-01-22 10:01:45 +0700

    BE-2553 [IE] Get script error when turn off options: Disable script debugging

--- common-v2/all-prefix.js
+++ common-v2/all-prefix.js
@@ -2,7 +2,7 @@
 // Prevent jQueryBE to be register in AMD
 // which cause conflict when client site use RequireJS
 if (typeof define === "function" && define.amd) {
-    window.tmpDefine = define; 
+    window.tmpDefine = define;
     define = undefined;
 }
 
@@ -10,5 +10,24 @@ if (typeof define === "function" && define.amd) {
     if (document.all && !document.addEventListener) {
         console.log('[PingOne] IE8 and below are not supported!');
         return;
+    }
+
+    if (typeof document.documentMode === 'number') {
+        if (document.documentMode <= 8) {
+            if (typeof console === 'object') {
+                if (typeof console.log === 'function') {
+                    console.log('[PingOne] IE8 and below are not supported!');
+                }
+            }
+            return;
+        } else {
+            // BE-2553
+            // When un-checking the setting, BHO can't re-init objects: window.extensions, window.messaging & window.accessible
+            // => request the user to restart their browser.
+            if (typeof window.extensions !== 'object' && typeof window.messaging !== 'object' && typeof window.accessible !== 'object') {
+                alert('[PingOne] Please restart your browser!');
+                return;
+            }
+        }
     }
     // START concatenate_files
