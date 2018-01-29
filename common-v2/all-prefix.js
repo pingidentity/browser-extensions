5e7a5980e16ba87842e3e9cde90667174cd12adf
NGOC HONG NGUYEN <nnguyen@pingidentity.com>
2018-01-29 16:10:55 +0700

    BE-2553: revert

--- common-v2/all-prefix.js
+++ common-v2/all-prefix.js
@@ -7,22 +7,8 @@ if (typeof define === "function" && define.amd) {
 }
 
 (function () {
-    if (typeof document.documentMode === 'number') {
-        if (document.documentMode <= 8) {
-            if (typeof console === 'object') {
-                if (typeof console.log === 'function') {
-                    console.log('[PingOne] IE8 and below are not supported!');
-                }
-            }
-            return;
-        } else {
-            // BE-2553
-            // When un-checking the setting, BHO can't re-init objects: window.extensions, window.messaging & window.accessible
-            // => request the user to restart their browser.
-            if (typeof window.extensions !== 'object' || typeof window.messaging !== 'object' || typeof window.accessible !== 'object') {
-                alert('[PingOne] Please restart your browser!');
-                return;
-            }
-        }
+    if (document.all && !document.addEventListener) {
+        console.log('[PingOne] IE8 and below are not supported!');
+        return;
     }
     // START concatenate_files
