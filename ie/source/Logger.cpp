d2ab8c9ca4cbabf3af50270b27b97fdfddf6a0f3
NGOC NGUYEN <nnguyen@pingidentity.com>
2018-03-20 10:03:27 +0700

    BE-2588: Log BE data into an official record for reviewing when issues happen

--- ie/source/Logger.cpp
+++ ie/source/Logger.cpp
@@ -157,7 +157,7 @@ void Logger::writeOnTab(const std::wstring& message, const std::wstring& onTabId
     if (m_tablogfolder != L"") {
         try {
             std::wofstream fsTab;
-            fsTab.open(m_tablogfolder + L"\\extension-" + onTabId + ".log", std::ios::out | std::ios::app);
+            fsTab.open(m_tablogfolder + L"\\extension-" + onTabId + L".log", std::ios::out | std::ios::app);
             #ifdef LOGGER_TIMESTAMP
                 timestampOnly(fsTab);
             #endif // LOGGER_TIMESTAMP
